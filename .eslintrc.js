57d4943d36b7c653a96ccc34374fa064ba207819
yangxun3 <yangxun3@Lenovo.com>
2023-03-09 14:36:48 +0800

    feat(custom): 新增husky代码格式校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,20 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es6: true,
   },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    sourceType: "module", // script或者module
+    ecmaVersion: "latest", // 6 7 8 9
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  globals: {
+    ROUTES: true,
   },
 };
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "*.{js,jsx,vue}": "vue-cli-service lint",
+  "*.{js,jsx,vue}": ["vue-cli-service lint", "eslint --fix", "git add ."],
 };
