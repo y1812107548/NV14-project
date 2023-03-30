c3f827dfbda21498ca1ec319cccf61d0f5fdfd01
yangxun3 <yangxun3@lenovo.com>
2023-03-30 11:03:19 +0800

    docs(custom): eslint备注

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     parser: "@babel/eslint-parser",
-    sourceType: "module", // script或者module
-    ecmaVersion: "latest", // 6 7 8 9
+    sourceType: "module", // script | module | undefined
+    ecmaVersion: "latest", // 3 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 2015 | 2016 | 2017 | 2018 | 2019 | 2020 | 2021 | 2022 | "latest" | undefined
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
